4ca14a72e35185aae81483bdf26a11f4ee8d9222
TeddyDharma <tedi.dharma84@gmail.com>
2024-04-23 16:35:38 +0800

    update main.py

--- main.py
+++ main.py
@@ -9,14 +9,16 @@ def load_model(model_dir):
     model = tf.keras.models.load_model(model_dir)
     return model
 
-
+def read_food_dataset(): 
+    return pd.read_excel("./data/recommendartion_data/final_data.xlsx")
 # gett the prediction
-def prediction_for_spesific_user(model, df: pd.DataFrame): 
+def prediction_for_spesific_user(): 
     # random user_id
     user_id = random.randint(a = 9999, b = 99999)
     all_prediction = []
     # recipe_id_uniques = np.unique(df.loc[:, 'resep_id'])
     model = load_model()
+    df = read_food_dataset()
     for i in range(df.shape[0]): 
         print(f'product id : {i}')
         all_prediction.append(model.predict(np.array([[user_id, i]]))[0][0] * 5)
